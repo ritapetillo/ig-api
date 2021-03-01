55ecb7c5aa382c551df2dfefd5c6a66fba1fee3d
ritapetillo <42487810+ritapetillo@users.noreply.github.com>
2021-03-01 06:14:00 -0500

    toJson and comparePass method added

--- src/Models/User.js
+++ src/Models/User.js
@@ -1,20 +1,49 @@
 const mongoose = require("mongoose");
+const bctypt = require("bcryptjs");
 
-const UserSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
+const UserSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    googleId: {
+      type: String,
+    },
+    IGId: {
+      type: String,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-  googleId: {
-    type: String,
-  },
-  IGId: {
-    type: String,
-  },
-});
+  {
+    toJSON: {
+      virtuals: true,
+      transform: function (doc, ret) {
+        delete ret._id;
+        delete ret.id;
+        delete ret.password;
+        delete ret.createdAt;
+        delete ret.updatedAt;
+        delete ret.googleId;
+        delete ret.IGId;
+        return ret;
+      },
+    },
+    timestamps: true,
+  }
+);
+
+UserSchema.methods.comparePass = async function (pass) {
+  try {
+    const isValid = await bcrypt.compare(pass, this.password);
+    return isValid;
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
+};
 
 module.exports = mongoose.model("users", UserSchema);
--- src/services/auth/index.js
+++ src/services/auth/index.js
@@ -1,11 +1,15 @@
-const express = require('express')
-const authRoutes = express.Router()
+const express = require("express");
+const authRoutes = express.Router();
+const User = require('../../Models/User')
 
-authRoutes.
+authRoutes.post("/login", async (req, res, next) => {
+    try {
+        const user = await User.findOne({ username })
+  } catch (err) {
+    const error = new Error("Wrong Credentials");
+    error.code = 401;
+    next(error);
+  }
+});
 
-
-
-
-
-
-module.exports = authRoutes
+module.exports = authRoutes;
