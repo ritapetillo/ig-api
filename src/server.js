975d47114f269a5acf6cc5d109e762daad95b08b
ritapetillo <42487810+ritapetillo@users.noreply.github.com>
2021-03-01 05:15:28 -0500

    boilerplate

---
+++ src/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const server = express();
+const error_handler = require("node-error-handler");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const apiRoutes = require("./services");
+const { PORT } = process.env;
+
+//MIDDLEWARES
+server.use(express.json());
+server.use(cookieParser());
+server.use(
+  cors({
+    origin: [
+      `${process.env.FRONT_URL}`,
+      "http://localhost:3002",
+      "http://localhost:3000",
+    ],
+    credentials: true,
+    exposedHeaders: ["set-cookie"],
+  })
+);
+
+//ROUTE
+server.use("/api", apiRoutes);
+
+//ERROR HANDLERS
+server.use(error_handler({ log: true, debug: true }));
+
+//Connect to DB and server
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    server.listen(PORT, () => {
+      console.log("server connected at port ", PORT);
+    })
+  );
