e896f889aaf1c97f827640556431bcaa3e45c6fc
ritapetillo <42487810+ritapetillo@users.noreply.github.com>
2021-03-01 06:47:47 -0500

    decode and verity tokens

---
+++ src/Middlewares/auth/tokens/index.js
@@ -0,0 +1,79 @@
+const { error } = require("console");
+const jwt = require("jsonwebtoken");
+const User = require("../../../Models/User");
+const {
+  ACCESS_TOKEN_SECRET,
+  REFRESH_TOKEN_SECRET,
+  EXPIRATION_ACCESS_TOKEN,
+  EXPIRATION_REFRESH_TOKEN,
+} = process.env;
+
+const encodeJWT = (payload, secret, expiration) =>
+  new Promise((res, rej) => {
+    jwt.sign(payload, secret, { expiresIn: expiration }, (err, token) => {
+      if (err) return rej(err);
+      else return res(token);
+    });
+  });
+
+const decodeJWT = (token, secret) =>
+  new Promise((res, rej) => {
+    jwt.verify(token, secret, (err, payload) => {
+      if (err) return rej(err);
+      else return res(payload);
+    });
+  });
+
+const generateTokens = async (user) => {
+  try {
+    const { username } = user;
+    const accessToken = await encodeJWT(
+      { username },
+      ACCESS_TOKEN_SECRET,
+      EXPIRATION_ACCESS_TOKEN
+    );
+    const refreshToken = await encodeJWT(
+      { username },
+      REFRESH_TOKEN_SECRET,
+      EXPIRATION_REFRESH_TOKEN
+    );
+    return { accessToken, refreshToken };
+  } catch (err) {
+    console.log(err);
+    return error;
+  }
+};
+
+const verifyAccessToken = async (token) => {
+  try {
+    const user = await decodeJWT(toekn, ACCESS_TOKEN_SECRET);
+    if (!user) return null;
+    else return user;
+    return user;
+  } catch (err) {
+    console.log(err);
+    return error;
+  }
+};
+
+const verifyRefreshToken = async (token) => {
+  try {
+    const user = await decodeJWT(toekn, REFRESH_TOKEN_SECRET);
+    if (!user) return null;
+    const { username } = user;
+    const savedRefreshToken = await User.findOne({
+      username,
+      refreshToken: token,
+    });
+    if (!savedRefreshToken) return null;
+    else return user;
+
+    return user;
+    return user;
+  } catch (err) {
+    console.log(err);
+    return error;
+  }
+};
+
+module.exports = { generateTokens, verifyAccessToken };
--- src/services/auth/index.js
+++ src/services/auth/index.js
@@ -1,10 +1,18 @@
+const { error } = require("console");
 const express = require("express");
 const authRoutes = express.Router();
-const User = require('../../Models/User')
+const User = require("../../Models/User");
 
 authRoutes.post("/login", async (req, res, next) => {
-    try {
-        const user = await User.findOne({ username })
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) throw error;
+    if (user) {
+        const isValid = user.comparePass(password);
+        //if it's valid, generate jwt
+        //send cookies
+    }
   } catch (err) {
     const error = new Error("Wrong Credentials");
     error.code = 401;
